396b592a28b678be49a71ae2423305a462692b6e
Mhedybeny01 <mehdibentaleb548@gmail.com>
2024-12-03 14:46:27 +0100

    Add files via upload

---
+++ pay.js
@@ -0,0 +1,13 @@
+const dropmenu = document.querySelector(".dropmenu"),
+  openMenu = document.querySelector(".openMenu"),
+  close = document.querySelector(".close");
+
+openMenu.addEventListener("click", () => {
+  dropmenu.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+  dropmenu.classList.remove("active");
+});
+
+
